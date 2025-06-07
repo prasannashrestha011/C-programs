64090d434fcb03e89f9300559b34bce389bd4f8d
prasannashrestha011 <shresthaprashanna27@gmail.com>
2025-06-07 19:24:31 +0545

    transpose of matrix

---
+++ matrix/transpose.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main() {
+  // 4x4 matrix
+  int a[2][2], i, j;
+  printf("Enter your numbers to build 4x4 matrix");
+  for (i = 0; i < 2; i++) {
+    for (j = 0; j < 2; j++) {
+      scanf("%d", &a[i][j]);
+    }
+  }
+  printf("Given matrix : \n");
+  for (i = 0; i < 2; i++) {
+    for (j = 0; j < 2; j++) {
+      printf("%d ", a[i][j]);
+    }
+    printf("\n");
+  }
+  printf("Your transpose matrix : \n");
+  for (i = 0; i < 2; i++) {
+    for (j = 0; j < 2; j++) {
+      printf("%d ", a[j][i]);
+    }
+    printf("\n");
+  }
+  return 0;
+}
